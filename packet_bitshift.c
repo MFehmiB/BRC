524897130ae21462b65111076aef2237def5bb96
MFehmiB <muhammedfb@gmail.com>
2018-11-06 20:48:30 +0100

    Rewrite the code to make it functional, remove unnecessary stuff and make it do what it should.

--- packet_bitshift.c
+++ packet_bitshift.c
@@ -1,56 +1,70 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 
-int main(int argc, char const *argv[]) {
-  /* code */
-  char *buffer, *dest;
-
-  //buffer = malloc(strlen(argv[1])+3);
-  //dest = buffer;
-  char *packet;
-  char original;
-  int newResult;
-  if (argc < 3) {
-    /* code */
-    printUsage();
-    return 0;
-  }
-  if (strlen(argv[1]) < 18) {
-    /* code */
-    printUsage();
-    return 0;
-  }
-
-  packet = argv[1];
-  newResult = *argv[2] -48;
-  //original = packet[12];
-  //printf("%s\n", original);
-  printf("%s\n", packet);
-  printf("%i\n", newResult);
-
-  //free(buffer);
-  return 0;
-}
-
-void printUsage(/* arguments */) {
-  /* code */
-  printf("This is a program to hack the results of your exam. (My teacher made me write it.)\n"
-  "Please enter two parameters: Firstly, your datapacket in single qoutes \'\'. \n"
+int printUsage() {
+  printf("Please enter two parameters: Firstly, your datapacket in single qoutes \'\'. \n"
   "Secondly, the test results you want to write in your datapacket as an int.\n\n"
-
   "Your datapacket should look like this: \n"
         "Inf-ITS*SS18?!!!Doe \n"
   "7 chars lecture code; 1 char exam number; 4-6 chars term; 1 char exampoints; \n"
   "3 chars student number; 2 or more chars; \n"
   "exam number, points and student number are ASCII-encoded, which means they will \n"
   "be shown as chars according to their corresponding ASCII symbol(s). \n");
+	return 0;
 }
 
 void print_bin(unsigned char paket) {
-    /* Byte (in Form von char buffer) als binärzahl auf der Konsole ausgeben */
-    for (int i = 0; i < 8; i++) {
-        printf("%d", (paket << i & 0x80) != 0);
-    }
-    printf("\n");
+	/* Byte (in Form von char buffer) als binärzahl auf der Konsole ausgeben */
+	for (int i = 0; i < 8; i++) {
+		printf("%d", (paket << i & 0x80) != 0);
+	}
+	printf("\n");
+}
+
+int main(int argc, char *argv[]) {
+	char* packet;
+	int newResult;
+	char byteToChange;
+	char newASCIIResult;
+
+	//Check if the packetsize is smaller than 18 byte (a char in C is always 1byte)
+	if (argc == 3) {
+		if (strlen(argv[1]) >= 18) {
+	  	packet = argv[1];
+			byteToChange = packet[12];
+		} else {
+			printf("Your Packet must be at least 18 Characters long! See usage:\n");
+			printUsage();
+			return 0;
+		}
+
+		//check if newResult is between 0-50
+		if (sscanf(argv[2], "%i", &newResult) == 1) {
+			if (newResult <= 50 && newResult >= 0) {
+			} else {
+				printf("Your Result must be between 0 - 50! See usage:\n");
+				printUsage();
+				return 0;
+			}
+		} else {
+			return -1;
+		}
+		printf("original:");
+		print_bin(byteToChange);
+		//bitshift to get points and also gets rid of flags
+		byteToChange = byteToChange>>2;
+		printf("points: %d\n", byteToChange);
+		newResult = newResult<<2;
+		//add flags
+		newResult = newResult+3;
+		printf("modifiziert:");
+		print_bin(newResult);
+		packet[12] = newResult;
+		printf("new packet name: %s", packet);
+  	return 0;
+	} else {
+		printf("Argument is missing! See usage:\n")
+		printUsage();
+		return 0;
+	}
 }
